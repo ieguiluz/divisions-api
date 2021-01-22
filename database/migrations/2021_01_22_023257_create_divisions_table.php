01ced1da13ec07eb0cd588993e085ed47c09bf5b
Irwin Eguiluz <irwin@bloom-ux.com>
2021-01-21 22:14:44 -0500

    first setup

---
+++ database/migrations/2021_01_22_023257_create_divisions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDivisionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('divisions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->integer('level');
+            $table->integer('quantity');
+            $table->string('ambassador')->nullable();
+            $table->timestamps();
+
+            $table->unique('name');
+            $table->foreign('parent_id')->references('id')->on('divisions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('divisions', function (Blueprint $table) {
+            dropForeign(['parent_id']);
+        });
+    }
+}
